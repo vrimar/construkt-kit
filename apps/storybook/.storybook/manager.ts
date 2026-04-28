217de4b0faf8a46997a64f6b719545c471e70691
Vasil Rimar <5660247+vrimar@users.noreply.github.com>
2026-04-28 12:08:52 -0400

    Add storybook logo

--- apps/storybook/.storybook/manager.ts
+++ apps/storybook/.storybook/manager.ts
@@ -4,11 +4,7 @@ import { create } from "storybook/theming/create";
 const construktKitTheme = create({
   base: "light",
   brandTitle: "Construkt Kit Design System",
-  brandImage:
-    "data:image/svg+xml," +
-    encodeURIComponent(
-      '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 120 40" width="120" height="40" fill="currentColor"><text x="0" y="32" font-family="system-ui, sans-serif" font-weight="700" font-size="38">ck</text></svg>',
-    ),
+  brandImage: "./images/logo.svg",
 });
 
 addons.setConfig({
